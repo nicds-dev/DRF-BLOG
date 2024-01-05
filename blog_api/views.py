73e3f5e2ed69d304ec90594ef87fe5696becc296
NicDs <nikolasdurango@gmail.com>
2024-01-04 22:23:33 -0500

    implemented permissions to the API views

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework import permissions
+
+class PostUserWritePermission(permissions.BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author.id == request.user.id
+        
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [permissions.DjangoModelPermissions]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
